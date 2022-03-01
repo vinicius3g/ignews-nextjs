d4db5b1638152ba3ee5dd3456162d70c0a0bd7ef
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-03-01 16:59:27 -0300

    Adding dynamic title

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Helo <span>World!</span>
-    </h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Helo <span>World!</span>
+      </h1>
+    </>
   );
 }
