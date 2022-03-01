c1772eb2a993c858abcf2edd822d8234c961a94b
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-03-01 16:08:42 -0300

    Added sass

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Helo World!</h1>;
+  return (
+    <h1 className={styles.title}>
+      Helo <span>World!</span>
+    </h1>
+  );
 }
