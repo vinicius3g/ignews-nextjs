652b54d8526910f7a0dc3a7527d6818cab580845
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-06-15 16:54:00 -0300

    feat: added home content and styles

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
+import Image from 'next/image'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Helo <span>World!</span>
-      </h1>
+
+      <main className={styles.contentcontainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>form $9.90 month</span>
+          </p>
+        </section>
+          <Image src="/images/avatar.svg" alt="Girl coding" width={500}
+        height={500}/>
+      </main>
     </>
   );
 }
