98a38a4c9587e41d4ce847129e2d8908e5b8c7fc
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-06-28 21:03:35 -0300

    feat(FaunaDB): integração do fauna com o app de auth do next salvando dados no banco

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -17,7 +20,25 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    })
+    }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: { email },
+          })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
