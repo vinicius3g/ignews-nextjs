f1f18f154e90ea87de3cde19c79b55489b7f54e0
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-07-02 15:12:17 -0300

    feat(Subscribe): gerando sessao do checkout

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from "next-auth/react";
 import { FC } from "react";
 
 import styles from "./styles.module.scss";
@@ -6,9 +7,18 @@ interface SubscribeButtonProps {
   priceId: string;
 }
 
-export const SubscribeButton: FC<SubscribeButtonProps> = ({priceId}) => {
+export const SubscribeButton: FC<SubscribeButtonProps> = ({ priceId }) => {
+  const { data: session }  = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
