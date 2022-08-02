95644cdbfb4067f4520b0a5c2733602d4c780365
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-08-01 22:01:02 -0300

    feat:  consumindo API do Prismic e Listando os Posts em tela

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
+  console.log({ posts });
   return (
     <>
       <Head>
@@ -10,38 +24,42 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
-          <a href="">
-            <time>12 de Maio de 2022</time>
-            <strong>Creating a Monorepo with Lerna</strong>
-            <p>mini paragrafo de explicação</p>
-          </a>
+          {posts.map((post) => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+//TODO: ajustar o retorno da api para pegar os resultados corretos
+//TODO: tarsnformar o metodo de captura dos dados para nova versao do nextJs
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getAllByType("post");
+
+  const posts = response?.map((post) => {
+    return {
+      slug: post.uid,
+      title: post.data.Title,
+      excerpt:
+        post.data.Content.find((content) => content.type === "paragraph")?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
---
+++ ignews/src/services/prismic.ts
@@ -0,0 +1,52 @@
+import * as Prismic from "@prismicio/client";
+import sm from "./../../sm.json";
+
+export function getPrismicClient() {
+  const endpoint = sm.apiEndpoint;
+
+  // const endpoint = Prismic.getRepositoryEndpoint(process.env.PRISMIC_REPO_NAME);
+
+  const prismic = Prismic.createClient(endpoint, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
+
+  return prismic;
+}
+
+//TODO:  mudar para GraphQL futuramente
+
+// import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+// import * as prismic from '@prismicio/client'
+
+// // Fill in your repository name
+// export const repositoryName = 'ignews-newdomain'
+
+// const prismicClient = prismic.createClient(
+//   prismic.getEndpoint(repositoryName),
+//   {
+//     // If your repository is private, add an access token
+//     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+
+//     // This defines how you will structure URL paths in your project.
+//     // Update the types to match the Custom Types in your project, and edit
+//     // the paths to match the routing in your project.
+//     //
+//     // If you are not using a router in your project, you can change this
+//     // to an empty array or remove the option entirely.
+//     routes: [
+//       {
+//         type: 'page',
+//         path: '/:uid',
+//       },
+//     ],
+//   }
+// )
+
+// export const client = new ApolloClient({
+//   link: new HttpLink({
+//     uri: prismic.getGraphQLEndpoint(repositoryName),
+//     fetch: prismicClient.graphQLFetch,
+//     useGETForQueries: true,
+//   }),
+//   cache: new InMemoryCache(),
+// })
