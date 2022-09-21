f3dcec50c631ff696279515a2f06033d471edf87
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2022-09-21 12:09:25 -0300

    feat: preview do post

---
+++ ignews/.slicemachine/assets/slices/Teste/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Teste';
+
+export default {
+  title: 'slices/Teste'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"teste","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent leading-edge vortals","spans":[]}],"description":[{"type":"paragraph","text":"Amet esse occaecat consectetur est magna. Laborum labore magna commodo laborum veniam enim esse exercitation ex Lorem reprehenderit elit.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
